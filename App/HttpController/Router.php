60b5d3d81c30b46470630cfe5d489879e824c35b
cozzz <zeco11320@163.com>
2018-12-11 18:48:06 +0800

    commit dev2

--- App/HttpController/Router.php
+++ App/HttpController/Router.php
@@ -7,8 +7,12 @@
  */
 
 namespace App\HttpController;
+
 use Lib\Http\AbstractInterface\AbstractRouter;
 use FastRoute\RouteCollector;
+use Lib\Http\Request;
+use Lib\Http\Response;
+
 class Router extends AbstractRouter
 {
     function initialize(RouteCollector $routeCollector)
@@ -16,7 +20,11 @@ class Router extends AbstractRouter
         // TODO: Implement initialize() method.
         $routeCollector->get('/test','/test/index');
         $routeCollector->get('/hide','/test/test');
+        $routeCollector->get('/','/');
 
         // TODO：Set Not Found & Not Allowed Call Method
+        $this->setRouterNotFoundCallBack(function(Request $request, Response $response) {
+            $response->write('<h1>未找到匹配的路由</h1>');
+        });
     }
 }
---
+++ lib/Data/Data.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cozz
+ * Date: 2018/12/11
+ * Time: 18:45
+ */
+
+namespace Lib\Data;
+
+
+class Data
+{
+
+}
